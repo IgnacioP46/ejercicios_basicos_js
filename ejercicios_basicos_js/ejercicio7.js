a732297fbbe1a48ae3085317ee9b56ed30c7cd1e
IgnacioP46 <169081304+IgnacioP46@users.noreply.github.com>
2024-06-25 15:22:56 +0200

    ejercicio7.js

--- ejercicios_basicos_js/ejercicio7.js
+++ ejercicios_basicos_js/ejercicio7.js
@@ -9,3 +9,10 @@ function greaterNumber(numberOne , numberTwo) {
         console.log(numberTwo);
     }
   }
+
+//o tambien usando:
+  function greaterNumber(numberOne, numberTwo) {
+    return Math.max(numberOne, numberTwo);
+}
+console.log(greaterNumber(5, 10)); //10
+console.log(greaterNumber(20, 15)); //20
