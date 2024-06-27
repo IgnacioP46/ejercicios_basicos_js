eef058df0ebe658f6314c23d067f7a05f21694fb
IgnacioP46 <169081304+IgnacioP46@users.noreply.github.com>
2024-06-27 17:59:44 +0200

    ejercicio14.js

--- ejercicios_basicos_js/ejercicio14.js
+++ ejercicios_basicos_js/ejercicio14.js
@@ -13,17 +13,13 @@ const words = [
   ];
   function repeatCounter(list) {
     // Completar
-    let count = {};
-  for (let i = 0; i < list.length; i++) {
-    let word = list[i];
-    if (count[word]) {
-      count[word]++;
-    } else {
-      count[word] = 1;
-    }
-  }
+    const count = {};
+  
+  list.forEach(word => {
+    count[word] = (count[word] || 0) + 1;
+  });
+  
   return count;
 }
 
 console.log(repeatCounter(words)); //{ code: 4, repeat: 1, eat: 1, sleep: 3, enjoy: 2 }
-  
